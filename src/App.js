97378b5724690694079d616b1995e7fd7c4a8eb9
Anish <anishrockagarwal1024@gmail.com>
2022-05-16 16:23:06 +0530

    Assignment completed and deployed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import FormData from "form-data";
+export default function App() {
+  let cnt=0,cntr=0,c="",co="";
+  const [text, setText] = useState("");
+  const [results, setResults] = useState([]);
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+  useEffect(() => {
+    var data = new FormData();
+    var numResults = 10;
+    var config = {
+      method: "get",
+      url: `https://asia-south1-socialboat-dev.cloudfunctions.net/assignmentVideos?q=${text}&numResults=${numResults}`,
 
-function App() {
+      data: data
+    };
+    if (text) {
+      axios(config)
+        .then(function (response) {
+          setResults(response.data.results);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }, [text]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="head">
+        <div className="logo">
+            <img className="logo-img" src="https://media.istockphoto.com/vectors/vector-design-element-for-the-fitness-center-vector-id1035561592?k=20&m=1035561592&s=612x612&w=0&h=8MwrSK9UQ_cY7K6NrKbi5txxK1R_Nhue0FGrw3L1BiI=" alt=""/>
+        </div>
+        <div className="search">
+            <input className="input" onChange={handleChange} type="text" name="search" placeholder="Search"id="search"/>
+                <button className="button" type="button">submit</button>
+        </div>
+        <div className="prop">
+            <img className="prop-img" src="https://media.istockphoto.com/vectors/vector-design-element-for-the-fitness-center-vector-id1035561592?k=20&m=1035561592&s=612x612&w=0&h=8MwrSK9UQ_cY7K6NrKbi5txxK1R_Nhue0FGrw3L1BiI=" alt=""/>
+        </div>
+    </div>
+      <div>
+      {text ? (
+        <div className="cardHolder">
+        {results.map((res) => {
+          return (
+            <div key={cntr++}>
+                <div key={cnt++} className="card">
+                  <div className="video-box">
+              <video key={c+="a"}controls>
+                <source src={res.video} type="video/mp4" />
+              </video>
+              </div>
+              <h3 key={co+="b"}>{res.heading.substring(text.length+1)}</h3>
+              <div className="tags">
+              {res.tags.map((tag) => {
+                return <span key={cntr++}>#{tag} </span>;
+              })}
+              </div>
+              </div>
+            </div>
+          );
+        })}
+        </div>
+      ) : (
+        <>
+          <p>Nothing to display</p>
+        </>
+      )}
+      </div>
     </div>
   );
 }
 
-export default App;
