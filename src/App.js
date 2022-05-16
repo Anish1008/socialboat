190771f1135add8062337159559dbb2385b666e6
Anish <anishrockagarwal1024@gmail.com>
2022-05-16 19:40:24 +0530

    Added filters by tag and debouncing

--- src/App.js
+++ src/App.js
@@ -6,28 +6,62 @@ export default function App() {
   let cnt=0,cntr=0,c="",co="";
   const [text, setText] = useState("");
   const [results, setResults] = useState([]);
+  const [timer,setTimer]= useState()
   function handleChange(e) {
+    // setTimer(setTimeout(()=>{
+    //   setText(e.target.value);
+
+    // },2000))
+    // if(timer)
+    // clearTimeout(timer);
     setText(e.target.value);
   }
+  const [uniqueTags,setUniqueTags] = useState([])
   useEffect(() => {
-    var data = new FormData();
-    var numResults = 10;
-    var config = {
-      method: "get",
-      url: `https://asia-south1-socialboat-dev.cloudfunctions.net/assignmentVideos?q=${text}&numResults=${numResults}`,
-
-      data: data
-    };
-    if (text) {
-      axios(config)
-        .then(function (response) {
-          setResults(response.data.results);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+    let timer = setTimeout(()=>{
+      
+      var data = new FormData();
+      var numResults = 100;
+      var config = {
+        method: "get",
+        url: `https://asia-south1-socialboat-dev.cloudfunctions.net/assignmentVideos?q=${text}&numResults=${numResults}`,
+  
+        data: data
+      };
+      if (text) {
+        axios(config)
+          .then(function (response) {
+            let tagArray=[];
+let unique =[]
+  let temp ="";
+  response.data.results.map((data)=>{
+    data.tags.map((res)=>{
+      tagArray.push(res)
+    })
+  })
+  tagArray.sort()
+  tagArray.map((data)=>{
+    if(data!==temp){
+      unique.push(data)
+      temp=data;
+    }
+  })
+  setUniqueTags(unique)
+            setResults(response.data.results);
+            console.log(response.data.results);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+    },2000)
+    return function(){
+      clearTimeout(timer)
     }
   }, [text]);
+  const [selected,setSelected]=useState("");
+  console.log(selected)
+  console.log(uniqueTags)
   return (
     <div className="App">
       <div className="head">
@@ -43,9 +77,20 @@ export default function App() {
         </div>
     </div>
       <div>
+          <h2 className="filter">Filter by Tags</h2>
+        <div className="Uniquetags">
+        {
+          uniqueTags.map((data)=>{
+          return<div onClick={()=>setSelected(data)}>{data}</div>
+          })
+        }
+        </div>
       {text ? (
+        <>
         <div className="cardHolder">
-        {results.map((res) => {
+        {  selected ? (
+        results.map((res) => {
+          if(res.tags.includes(selected)){
           return (
             <div key={cntr++}>
                 <div key={cnt++} className="card">
@@ -56,20 +101,42 @@ export default function App() {
               </div>
               <h3 key={co+="b"}>{res.heading.substring(text.length+1)}</h3>
               <div className="tags">
+
               {res.tags.map((tag) => {
                 return <span key={cntr++}>#{tag} </span>;
               })}
               </div>
               </div>
             </div>
-          );
-        })}
+          );}
+        })): (
+          results.map((res) => {
+            return (
+            <div key={cntr++}>
+                <div key={cnt++} className="card">
+                  <div className="video-box">
+              <video key={c+="a"}controls>
+                <source src={res.video} type="video/mp4" />
+              </video>
+              </div>
+              <h3 key={co+="b"}>{res.heading.substring(text.length+1)}</h3>
+              <div className="tags">
+
+              {res.tags.map((tag) => {
+                return <span key={cntr++}>#{tag} </span>;
+              })}
+              </div>
+              </div>
+            </div>
+)}))}
         </div>
+      </>
       ) : (
         <>
           <p>Nothing to display</p>
         </>
-      )}
+      )
+      }
       </div>
     </div>
   );
